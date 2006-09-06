f12b2a50237e407001c306636daf9c49fe6bf785
Benji York <benji@benjiyork.com>
2006-09-06 15:42:38 +0000

    - remove duplicate "zip_safe" directive  - whitespace cleanup

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ setup(
     install_requires=['zc.resourcelibrary >= 0.5'],
     dependency_links=['http://download.zope.org/distribution/',],
     packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
-    
+
     package_dir= {'':'src'},
-    
+
     namespace_packages=['zc'],
     package_data = {
     '': ['*.txt', '*.zcml', '*.gif', '*.js'],
     'zc.table':['resources/*', '*.pt'],
     },
 
-    zip_safe=False,
     author='Zope Project',
     author_email='zope3-dev@zope.org',
     description=open("README.txt").read(),
