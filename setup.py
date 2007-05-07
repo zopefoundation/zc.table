554aaeb4fb3294f9cf57ad7ae822611af814dee7
Christian Zagrodnick <cz@gocept.com>
2007-05-07 07:43:24 +0000

    - removed the zope3 checkout from the buildout
    - set the dependencies right so the tests get all their eggs

--- setup.py
+++ setup.py
@@ -3,7 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="zc.table",
     version="0.7dev",
-    install_requires=['setuptools', 'zc.resourcelibrary >= 0.6'],
+    install_requires=[
+        'setuptools',
+        'zc.resourcelibrary >= 0.6',
+        'zope.app.form',
+        'zope.app.testing',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.i18n',
+        'zope.interface',
+        'zope.schema',
+        'zope.testing',
+    ],
     packages=find_packages('src'),
     package_dir= {'':'src'},
 
