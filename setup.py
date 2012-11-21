6839508d793dcef9d4e5799ea9a7d3d3e67102cb
Gediminas Paulauskas <menesis@pov.lt>
2012-11-21 15:38:19 +0000

    Preparing release 0.9.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name="zc.table",
-    version="0.9.0dev",
+    version='0.9.0',
     url="http://pypi.python.org/pypi/zc.table/",
     install_requires=[
         'setuptools',
