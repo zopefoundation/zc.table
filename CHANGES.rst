37b6fa9b78f5071b9b5ec70c5c153d4f2fa601b6
Michael Howitz <mh@gocept.com>
2023-02-09 15:47:04 +0100

    Bumped version for breaking release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,8 @@
 CHANGES
 =======
 
-0.11 (unreleased)
------------------
+1.0 (unreleased)
+----------------
 
 - Add support for Python 3.5, 3.7, 3.8 and 3.9.
 
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup(
     name="zc.table",
-    version='0.11.dev0',
+    version='1.0.dev0',
     url="https://github.com/zopefoundation/zc.table/",
     install_requires=[
         'setuptools',
